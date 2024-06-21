60d6c24a1e28ea4fe8d898f40951c20d00116ee5
Oleksandr <o.tymoshenko@astoundcommerce.com>
2024-06-21 13:34:05 +0300

    add streaming

--- asistants/create_new_thread_with_existing_assistant.py
+++ asistants/create_new_thread_with_existing_assistant.py
@@ -1,10 +1,34 @@
 from openai import OpenAI
+from typing_extensions import override
+from openai import AssistantEventHandler
 import os
 from dotenv import load_dotenv
 import re
 import sys
 
 load_dotenv()
+
+class EventHandler(AssistantEventHandler):
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
@@ -28,7 +52,21 @@ message = client.beta.threads.messages.create(
 
 print("thread ID: " + thread.id)
 
-# start main process
+# Then, we use the `stream` SDK helper
+# with the `EventHandler` class to create the Run
+# and stream the response.
+
+# start main process with streaming
+with client.beta.threads.runs.stream(
+  thread_id=thread.id,
+  assistant_id=assistant_id,
+  instructions=os.getenv("RUN_INSTRUCTION"),
+  event_handler=EventHandler(),
+) as stream:
+  stream.until_done()
+
+'''
+# start main process without streaming
 run = client.beta.threads.runs.create_and_poll(
   thread_id=thread.id,
   assistant_id=assistant_id,
@@ -46,3 +84,4 @@ if run.status == 'completed':
   print(cleaned_content)
 else:
   print(run)
+'''
